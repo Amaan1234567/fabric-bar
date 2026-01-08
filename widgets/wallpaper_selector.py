48fbbf615109fa50d5b09818f3129993e5376d20
Abdul Amaan <amaan.programmer1@gmail.com>
2026-01-08 23:56:51 +0530

    feat: enhance wallpaper selector with new layout and dynamic wallpaper processing

--- widgets/wallpaper_selector.py
+++ widgets/wallpaper_selector.py
@@ -1,14 +1,30 @@
-
-from wsgiref.util import is_hop_by_hop
+import os
+from screeninfo import get_monitors
+from PIL import Image
 from fabric.widgets.wayland import WaylandWindow as Window
 from fabric.widgets.box import Box
 from fabric.widgets.label import Label
 from fabric.widgets.button import Button
 from fabric.widgets.scrolledwindow import ScrolledWindow
+from helpers.helper_functions import truncate
+
+scale_map = {
+    1920: 1280,
+    2560: 1080,
+    3840: 2560,
+}
+
 
 class WallpaperSelector(Window):
     """wallpaper selector widget"""
-    def __init__(self,**kwargs):
+
+    def __init__(self, **kwargs):
+        monitor = get_monitors()[0]
+        self.screen_width = monitor.width
+        self.screen_height = monitor.height
+        self.preview_target_width = int(scale_map[self.screen_width] * 0.2)
+        self.preview_target_height = int(scale_map[self.screen_width] * (9 / 16))
+
         super().__init__(
             name="wallpaper-selector-window",
             title="wallpaper-selector",
@@ -17,21 +33,85 @@ class WallpaperSelector(Window):
             exclusivity="auto",
             keyboard_mode="on-demand",
             visible=False,
-            type="popup",**kwargs
+            type="popup",
+            **kwargs,
         )
 
-        self.content = Box(name="wallpaper-selector-container",)
-        self.scrolling_container = ScrolledWindow(name="wallpaper-selector-scroll-container")
-        self.label1 = Button(label="testing1",on_clicked=lambda _: print("pressed "))
-        self.label2 = Button(label="testing2",on_clicked=lambda _: print("pressed "))
-        self.label3 = Button(label="testing3",on_clicked=lambda _: print("pressed "))
-        self.box = Box(orientation="h",spacing=20,children=[self.label1,self.label2,self.label3])
+        self.content = Box(name="main-wallpaper-container",h_expand=True,v_expand=True,
+            size=[self.preview_target_width * 5,self.preview_target_height + 90],
+            
+        )
+        self.wallpaper_folder = f"{os.environ.get('HOME')}/Pictures/backgrounds/"
+        self.cache_folder = f"{os.environ.get('HOME')}/.cache/wallpapers_cache/"
+        self.wallpapers = os.listdir(self.wallpaper_folder)
+        self.cache = os.listdir(self.cache_folder)
+        self._process_new_wallpapers()
 
-        self.scrolling_container.children = [self.box]
+        self.scrolling_container = ScrolledWindow(
+            name="wallpaper-scroll-container",
+            max_content_size=[
+                self.preview_target_width * 5,
+                self.preview_target_height + 300,
+            ],
+        )
+
+        self.buttons_box = Box(name="wallpapers-container",orientation="h",h_expand=True,v_expand=True, spacing=20)
+        self._create_buttons()
+
+        self.scrolling_container.children = [self.buttons_box]
         self.content.add(self.scrolling_container)
         self.children = [self.content]
-        self.is_hidden=True
-    
+        self.is_hidden = True
+        
+        
+
+    def _process_new_wallpapers(self):
+        for wallpaper in self.wallpapers:
+            if wallpaper not in self.cache:
+                self._process_wallpaper(wallpaper)
+
+    def _process_wallpaper(self, wallpaper: str):
+        scaled_wallpaper = Image.open(self.wallpaper_folder + wallpaper).resize(
+            (
+                scale_map[self.screen_width],
+                int(scale_map[self.screen_width] * (9 / 16)),
+            ),
+            Image.Resampling.BILINEAR,
+        )
+        left = scaled_wallpaper.size[0] // 2 - int(scaled_wallpaper.size[0] * 0.1)
+        right = scaled_wallpaper.size[0] // 2 + int(scaled_wallpaper.size[0] * 0.1)
+        bottom = scaled_wallpaper.size[1]
+        top = 0
+        cropped_image = scaled_wallpaper.crop((left, top, right, bottom))
+        cropped_image.save(self.cache_folder + wallpaper)
+
+    def _create_buttons(self):
+        print(os.listdir(self.cache_folder))
+        images = filter(lambda name: ".gif" not in name, os.listdir(self.cache_folder))
+
+        for image_file_name in images:
+            print(image_file_name)
+            image = Box(
+                name="wallpaper-thumnail",
+                size=[self.preview_target_width, self.preview_target_height],
+            )
+            image.set_style(
+                f"""border-radius: 10px;
+                background-image: url('file://{self.cache_folder + image_file_name}');""",
+                compile=False
+            )
+            label = Label(label=truncate(image_file_name), name="wallpaper-label")
+            box = Box(
+                name="wallpaper-container",
+                orientation="v",
+                children=[image, label],
+                h_align="center",
+                v_align="center",
+                h_expand=True,v_expand=True
+            )
+            button = Button(name="wallpaper-button", child=box,h_expand=True,v_expand=True)
+            self.buttons_box.add(button)
+
     def toggle_window(self):
         """function to toggle window"""
         if self.is_hidden:
