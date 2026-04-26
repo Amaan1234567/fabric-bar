183b919491d63e6c8ef0eeab3801fd2a5865615c
Abdul Amaan <amaan.programmer1@gmail.com>
2026-04-26 16:17:20 +0530

    refactor: enhance ScrollingLabel with dynamic animation duration and improved pause handling

--- custom_widgets/scolling_text_widget.py
+++ custom_widgets/scolling_text_widget.py
@@ -1,38 +1,75 @@
+from functools import partial
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Pango', '1.0')
 gi.require_version('PangoCairo', '1.0')
 from gi.repository import Gtk, GLib, Pango, PangoCairo
 
+from utils.animator import Animator, cubic_bezier
+
 class ScrollingLabel(Gtk.DrawingArea):
-    def __init__(self, text="---", speed=0.4, max_width=200, **kwargs):
+    def __init__(self, text="---", bezier =(0, 0.37, 1, 0.65),speed=0.8, pause_ms=2000, max_width=200, **kwargs):
         super().__init__(**kwargs)
         self.set_name("song-title")
         self.text = text
         self.speed = speed
         self.max_width_limit = max_width
+        self.pause_ms = pause_ms 
         
-        # Prevent the widget from stretching to fill its parent
         self.set_halign(Gtk.Align.START) 
         
-        self.x_offset = 0
-        self.direction = -1
-        self.pause_counter = 0
+        self._pause_source_id = None
         
-        # GLib.timeout_add(16, self.update_position)
-        self.add_tick_callback(self.update_position)
+        # Initialize Animator with a dummy duration (it will be updated dynamically)
+        self.animator = Animator(
+            duration=1.0, 
+            timing_function=partial(cubic_bezier,*bezier),
+            min_value=0.0,
+            max_value=1.0,
+            repeat=False,
+            tick_widget=self
+        )
+        
+        self.animator.connect("notify::value", self.on_animator_step)
+        self.animator.connect("finished", self.on_animator_finished)
+
+    def on_animator_step(self, animator, *args):
+        self.queue_draw()
+
+    def on_animator_finished(self, *args):
+        # Swap min/max to reverse direction
+        current_min = self.animator.min_value
+        current_max = self.animator.max_value
+        self.animator.min_value = current_max
+        self.animator.max_value = current_min
+        
+        if self._pause_source_id:
+            GLib.source_remove(self._pause_source_id)
+            
+        self._pause_source_id = GLib.timeout_add(self.pause_ms, self._resume_animation)
+
+    def _resume_animation(self):
+        self._pause_source_id = None
+        self.animator.play()
+        return GLib.SOURCE_REMOVE
 
     def get_text(self):
-        """ Returns the current text string. """
         return self.text
 
     def set_text(self, new_text):
-        """ Updates text and resets scrolling state. """
         if self.text != str(new_text):
             self.text = str(new_text)
-            self.x_offset = 0
-            self.direction = -1
-            self.pause_counter = 0
+            
+            # Reset Animator State
+            self.animator.pause()
+            if self._pause_source_id:
+                GLib.source_remove(self._pause_source_id)
+                self._pause_source_id = None
+                
+            self.animator.min_value = 0.0
+            self.animator.max_value = 1.0
+            self.animator.value = 0.0
+            
             self.queue_resize()
 
     def do_get_preferred_width(self):
@@ -41,7 +78,6 @@ class ScrollingLabel(Gtk.DrawingArea):
         layout.set_font_description(style_context.get_font(Gtk.StateFlags.NORMAL))
         text_w, _ = layout.get_pixel_size()
         
-        # Enforce max width limit
         natural = min(text_w, self.max_width_limit)
         return natural, natural 
 
@@ -64,36 +100,39 @@ class ScrollingLabel(Gtk.DrawingArea):
         layout.set_font_description(font_desc)
         text_w, text_h = layout.get_pixel_size()
 
-        # Define visible area
         cr.rectangle(0, 0, width, height)
         cr.clip()
 
         y_pos = (height - text_h) / 2
 
-        # Bounce Logic
         if text_w > width:
             max_scroll = width - text_w - 4
-            if self.pause_counter <= 0:
-                self.x_offset += (self.speed * self.direction)
-                if self.x_offset <= max_scroll:
-                    self.x_offset = max_scroll
-                    self.direction = 1
-                    self.pause_counter = 187.5
-                elif self.x_offset >= 0:
-                    self.x_offset = 0
-                    self.direction = -1
-                    self.pause_counter = 187.5
-            else:
-                self.pause_counter -= 1
+            scroll_distance = abs(max_scroll)
+            
+            # Convert speed (pixels per 16ms tick) to pixels per second
+            pixels_per_second = self.speed * (1000 / 16)
+            
+            # Dynamically calculate the duration needed to cover the distance at the given speed
+            target_duration = scroll_distance / pixels_per_second
+            
+            # Update the animator's duration if it has changed (e.g., due to resize or new text)
+            if abs(self.animator.duration - target_duration) > 0.01:
+                self.animator.duration = target_duration
+
+            if not self.animator.playing and self._pause_source_id is None:
+                self.animator.play()
+            
+            x_offset = max_scroll * self.animator.value
         else:
-            self.x_offset = 0
+            self.animator.pause()
+            if self._pause_source_id:
+                GLib.source_remove(self._pause_source_id)
+                self._pause_source_id = None
+            self.animator.value = 0.0
+            self.animator.min_value = 0.0
+            self.animator.max_value = 1.0
+            x_offset = 0
 
-        # Paint using CSS color
         cr.set_source_rgba(rgba.red, rgba.green, rgba.blue, rgba.alpha)
-        cr.move_to(self.x_offset, y_pos)
+        cr.move_to(x_offset, y_pos)
         PangoCairo.show_layout(cr, layout)
-
-    def update_position(self, widget, frame_clock):
-        self.queue_draw()
-        # 3. Return Continue to keep the loop going
-        return GLib.SOURCE_CONTINUE
