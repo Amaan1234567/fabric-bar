35b4689f791ac71d7160e09e927a62120599f87c
Abdul Amaan <amaan.programmer1@gmail.com>
2026-05-09 14:05:35 +0530

    fix: set spacing for SystemTray widget in barSystemTray

--- modules/system_tray/system_tray.py
+++ modules/system_tray/system_tray.py
@@ -5,5 +5,5 @@ from fabric.system_tray.widgets import SystemTray
 class barSystemTray(Box):
     def __init__(self, name="system-tray", **kwargs):
         super().__init__(**kwargs)
-        self.tray = SystemTray()
+        self.tray = SystemTray(spacing=10)
         self.add(self.tray)
